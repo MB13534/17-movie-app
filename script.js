0fc41a1a10c0e105d11e9b4f078d8500f0dd83ce
Michael <michael.barry.email@gmail.com>
2020-09-29 18:55:53 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const key = 'be12910da2f9b8c1e5c95ac22f8e1209'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=be12910da2f9b8c1e5c95ac22f8e1209&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=be12910da2f9b8c1e5c95ac22f8e1209&query="'
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+//get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const response = await fetch(url) 
+    const data = await response.json()
+console.log(data.results)
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    
+    movies.forEach(movie => {
+        const { poster_path, title, overview, vote_average } = movie;
+        
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl);
+    })
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green"
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const searchTerm = search.value;
+    
+if (searchTerm && searchTerm !== ''){
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+}else {
+    window.location.reload();
+}
+
+});
+
